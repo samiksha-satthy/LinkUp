508841571d76b9a6d3934b9a10b7718efe93ce15
samiksha-satthy <satthy.samiksha@gmail.com>
2025-04-07 02:43:55 -0400

    frontend: can like and add comments

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -7,6 +7,7 @@ import MessagesView from '@/views/MessagesView.vue'
 import SearchView from '@/views/SearchView.vue'
 import ProfileView from '@/views/ProfileView.vue'
 import FriendsView from '@/views/FriendsView.vue'
+import PostView from '@/views/PostView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -58,6 +59,11 @@ const router = createRouter({
       path: '/profile/:id/friends', 
       name: 'friends', 
       component: FriendsView,
+    },
+    {
+      path: '/:id', 
+      name: 'postview', 
+      component: PostView,
     },
   ],
 })
